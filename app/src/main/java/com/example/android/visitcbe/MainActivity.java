cd958c7b8a256ec406b7220b465b6834339f57b7
Sritharini <15s148@psgitech.ac.in>
2017-06-22 12:22:15 +0530

    Add ListView and ArrayAdapter to the TempleActivity.java

--- app/src/main/java/com/example/android/visitcbe/MainActivity.java
+++ app/src/main/java/com/example/android/visitcbe/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,24 +13,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
 
-    //Using intent to move from main acticity to clicked acticity
-    public void openTempleList(View view) {
-        Intent i = new Intent(this, TempleActivity.class);
-        startActivity(i);
-    }
-    public void openFoodList(View view) {
-        Intent i = new Intent(this, FoodActivity.class);
-        startActivity(i);
-    }
-    public void openCollegeList(View view) {
-        Intent i = new Intent(this, CollegeActivity.class);
-        startActivity(i);
-    }
-    public void openTouristPlacesList(View view) {
-        Intent i = new Intent(this, TouristsplacesActivity.class);
-        startActivity(i);
+        // Find the View that shows the temples category
+        TextView temples = (TextView) findViewById(R.id.temple);
+
+        // Set a click listener on that View
+        temples.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent templesIntent = new Intent(MainActivity.this, TempleActivity.class);
+                startActivity(templesIntent);
+            }
+        });
+        // Find the View that shows the numbers category
+        TextView food = (TextView) findViewById(R.id.food);
+
+        // Set a click listener on that View
+        food.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent foodsIntent = new Intent(MainActivity.this, FoodActivity.class);
+                startActivity(foodsIntent);
+            }
+        });
+        // Find the View that shows the numbers category
+        TextView college = (TextView) findViewById(R.id.college);
+
+        // Set a click listener on that View
+        college.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent collegeIntent = new Intent(MainActivity.this, CollegeActivity.class);
+                startActivity(collegeIntent);
+            }
+        });
+        // Find the View that shows the numbers category
+        TextView touristPlaces = (TextView) findViewById(R.id.touristPlaces);
+
+        // Set a click listener on that View
+        touristPlaces.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent touristsPlacesIntent = new Intent(MainActivity.this, TouristsplacesActivity.class);
+                startActivity(touristsPlacesIntent);
+            }
+        });
+
     }
 }
+
+
--- app/src/main/java/com/example/android/visitcbe/TempleActivity.java
+++ app/src/main/java/com/example/android/visitcbe/TempleActivity.java
@@ -2,6 +2,11 @@ package com.example.android.visitcbe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by LENOVO on 21-06-2017.
@@ -13,5 +18,29 @@ public class TempleActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temple);
+
+        ArrayList<String> info = new ArrayList<String>();
+        info.add("Perur Pateeshwarar Temple");
+        info.add("Marudhamalai Murugan Temple");
+        info.add("Koniamman Temple");
+        info.add("Thandumaariyamman Temple");
+        info.add("Eachanaari Vinayagar Temple");
+        info.add("Saibaba Temple");
+        info.add("Sri A shtaamsha varadha aanjaneyar Temple");
+        info.add("Isha");
+        info.add("Iscon");
+        info.add("Iyyapan Temple");
+
+
+        //simple_list_item_1 is defined in android framework which contains single TextView which the
+        // adapter is set to display a word
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,info);
+
+        //Find the oblect ListView in the view hierarchy of Activity
+        //There should be a ListView with a view id list in the activity_temple.xml
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //Call setAdapter on ListView object and pass an argument which is arrayAdapter
+        listView.setAdapter(arrayAdapter);
     }
 }
